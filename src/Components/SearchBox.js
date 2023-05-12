31529a451921d3b4ed43f1d14e9667859921b138
Lynard Salingujay <lynard.salingujay@gentrack.com>
2023-05-13 00:13:56 +1200

    added Components

---
+++ src/Components/SearchBox.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function SearchBox() {
+   return (
+      <div className='search'>
+         <div className='searchInput'><input type="text" /></div>
+         <div className='searchDisplay'></div>
+      </div>
+   )
+}
+
+export default SearchBox
